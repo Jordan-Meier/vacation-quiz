8dc98e124ddaa52d38386756cca2e911453c8e3b
Jordan <jbirdmeier@gmail.com>
2016-01-15 10:52:58 -0800

    Add the variable of 'total' to scripts.js. Because I set my option values to be numeric in my form, I only need one variable (the sum of those numeric values) to determine the destination.

--- js/scripts.js
+++ js/scripts.js
@@ -4,7 +4,13 @@ $(document).ready(function() {
     $("#tuscany").hide();
     $("#buenos-aires").hide();
     $("#tokyo").hide();
-    
+    var total = parseInt($("select#weather").val());
+      total += parseInt($("select#food").val());
+      total += parseInt($("select#activity").val());
+      total += parseInt($("select#music").val());
+      total += parseInt($("select#movie").val());
+
+      console.log(total);
   });
 
   event.preventDefault();
